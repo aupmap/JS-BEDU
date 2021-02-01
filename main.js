7fc9c84944f5fa10d5c013c26459d8965213b3de
aurora <padillaurora@gmail.com>
2021-02-01 13:43:58 -0600

    Ejemplos sesion 3

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 /*
+// Ejemplo Switch
 var day = 5;
 var text;
 
@@ -30,6 +31,7 @@ switch (day) {
 
 }
 
+//Ejemplo while-for
 var i = 0;
 
 while(i <= 200) {
@@ -56,12 +58,17 @@ for(var i = 0; i <= 100; i++) {
       console.log(counter);
     }
 
+
+//Funcion ejemplo 1
 function saludar(saludo, name) {
     alert(saludo + ' ' + name);
 }
 
-saludar('Hola mundo', 'usuario')
+saludar('Hola mundo', 'Au')
 
+
+
+//Funcion ejemplo 2
 
 function perimeter(a,b) {
     alert(a*b);
@@ -69,20 +76,44 @@ function perimeter(a,b) {
 perimeter(2, 3)
 
 var i = 0;
-*/
+
+
+//Funcion ejemplo 3
+
+var square = function(number) {
+	return number * number;
+}
+
+var squareOfFour = square(4);
+
+console.log(squareOfFour); // 16
+
+//Funcion ejemplo  4
+
+var factorial = function fac(number) {
+	return number < 2 ? 1 : number * fac(number - 1)
+}
+
+console.log(factorial(5)); // 120
+
 
 //Declaracion de funciones
+
+
+
 const hola= 'hola usuario';
 
 function saludar(saludo, name= 'Pedro') {
     //hola = 'adios';
-    //alert(saludo + ' ' + ' ' + 'hola' + name)
+    alert(saludo + ' ' + ' ' + 'hola' + name)
     console.log(saludo + ' ' + name);
 
-    //return saludo + ' ' + name;
+    return saludo + ' ' + name;
 }
 
 saludar('Hola mundo')
+
+
 
 let edad = 16;
 
@@ -92,6 +123,7 @@ if (edad > 18) {
 } else {
     console.log('Ciudad', ciudad);
 }
+
 
 function padre(par1, par2) {
     console.log('par1', par1);
@@ -104,6 +136,56 @@ function padre(par1, par2) {
    // console.log(para1);
 }
 
+
 padre(23,'ul');
 
 padre.hija('adios');
+
+
+function calculateAge(birthYear) {
+  var age = 2021 - birthYear;
+  return age;
+}
+
+var ageAu = calculateAge(1986);
+
+console.log(ageAu); // 35
+
+
+
+function yearsUntilRetirement(year, name) {
+  var age = calculateAge(year);
+  var retirement = 65 - age;
+  console.log(name + ' retires in ' + retirement + ' years.');  // Au retires in 30 years.
+}
+
+yearsUntilRetirement(1986, 'Au');
+
+
+
+function power(base, exponent) {
+  var result = 1;
+  
+  for(var i = 0; i < exponent; i++) {
+      result *= base; // Same as: result = result * base;
+  }
+  
+  return result;
+}
+
+*/
+
+function fibonacci(num) {
+  if (num <= 1) return 1;
+
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+function fibonacciSequence(limit) {
+  if(limit < 1) return console.log('Limit must be greater than 0');
+
+  for(var i = 0; i < limit; i++ ) {
+    console.log( fibonacci(i) );
+  }
+}
+fibonacciSequence(6);
