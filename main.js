3d4850565e4bccfa60fa3a8a3f9658ccb02bf368
aurora <padillaurora@gmail.com>
2021-02-02 22:04:12 -0600

    Ultimos cambios sesion4

--- main.js
+++ main.js
@@ -4,8 +4,54 @@
 (function(){
   console.log('hola desde IIFE')
 })()
-*/
 
 //const entrada = Number(prompt('Proporciona tu edad:'));
 
 //console.log(Mensaje: ', entrada');
+
+let lista = [0,3,46,7,'antonio',[0,2,2],'ul',4]
+
+console.log(lista[9]);
+
+//lista.length
+//lista['lenght']
+
+//lista.push()
+//lista[]
+
+//lista.splice(4,5, 'Nuevo', 'otro')
+console.error('Hola amigos')
+console.warn('Hola amigos')
+
+lista.unshift(78);
+console.warn(lista)
+console.warn(nuevo)
+
+lista.push(67)
+console.warn(lista)
+
+lista[200]='nuevo';
+console.warn(lista)
+console.warn(lista.length)
+console.warn(lista[4])
+
+let ultima = lista.pop();
+console.warn(ultima)
+console.warn(lista)
+
+let primero = lista.shift();
+console.warn(lista)
+console.warn(primero)
+
+
+///
+
+console.log(lista.index0f('antonio'))
+console.log(lista.lastIndex0f('antonio'))
+//console.log(lista.lastIndex0f('antonio'))
+console.log(lista.includes('pedro'))
+
+console.log(lista)
+lista.splice(4,5)
+
+console.log(lista)
